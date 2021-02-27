73417163e19badba3b5a1a93ae795152b6aa9061
Lakshitha Abeywickrama <lakshitha129@gmail.com>
2021-02-28 03:07:11 +1100

    Update script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+console.log("Hello Operators!!");
+
+const a = 5;
+const b = 10;
+let result = 0;
+
+// Arithmetic Operators //
+
+//Addition operator
+result = a + b;
+console.log(result);
+
+//Substraction operator
+result = a - b;
+console.log(result);
+
+//Multiplication operator
+result = a * b;
+console.log(result);
+
+//Division operator
+result = a / b;
+console.log(result);
+
+//Exponnt operator
+result = b ** a;
+console.log(result);
+
+//Modulo operator = 11 divided by 10 and whats the remainder
+result = 11 % 10;
+console.log(result);
+
+
+// Comparison Operators -- Always retun Boolean values TRUE and FALSE//
+
+// Less than Operator
+result = a < b;
+console.log(result);
+
+//Less than or equal operator
+result = a <= b;
+console.log(result);
+
+//Greater than operator
+result = a > b;
+console.log(result);
+
+//Greater than or equal operator
+result = a >= b;
+console.log(result);
+
+// Is equal to operator -- loose Equality
+result = a == b;
+console.log(result);
+
+// Is NOT equal to operator -- loose
+result = a != b;
+console.log(result);
+
+// Strict Equality Operator
+result = a === b;
+console.log(result);
+
+// Strict Inequality Operator
+result = a !== b;
+console.log(result);
