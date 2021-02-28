4bd6a31782216bcec908caaa56e334034157136c
Lakshitha Abeywickrama <lakshitha129@gmail.com>
2021-02-28 14:48:31 +1100

    Update script.js

--- script.js
+++ script.js
@@ -64,3 +64,45 @@ console.log(result);
 // Strict Inequality Operator
 result = a !== b;
 console.log(result);
+
+// Strict Equality compares the VALUES and DATA TYPE then returns the result in BOOLEAN (True or False
+// Lose Equality compares the VALUES ONLY
+
+// The Good ones are : === and !==
+// The Evil Twins Are : == and !=
+
+
+// Logical Operators -- OR , AND , NOT//
+
+// AND && = Returns TRUE if all OPERANDS are TRUE
+console.log( true && true );
+console.log( false && false );
+console.log( true && false );
+console.log( true && true && true);
+console.log( false && true && false);
+
+// OR || = Returns TRUE if at least one OPERANDS is TRUE
+console.log( true || false );
+console.log( false || false );
+console.log( true || true);
+console.log( false|| true || false );
+console.log( true || true || true );
+
+// NOT ! = Converts the BOOLEAN value to the inverse - TRUE to FALSE and FALSE to TRUE
+console.log(!false);
+console.log(!true);
+console.log(!true && !false);
+console.log(!false && !false);
+
+
+// Assignment Operators = , += , -= , *= , /= 
+
+let _number = 6 ;
+console.log( _number );
+
+_number = 5 ;
+console.log( _number );
+console.log(_number += 5);
+console.log(_number -= 5);
+console.log(_number*=5);
+console.log(_number/=5);
